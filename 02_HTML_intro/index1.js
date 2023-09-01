aab16ab6975a8d268edce0ac72db47b451e26933
Uma Shah <uma000@gmail.com>
2023-09-01 10:43:07 +0100

    added operators

---
+++ 02_HTML_intro/index1.js
@@ -0,0 +1,13 @@
+let numTest = 45.324568;
+console.log(numTest);
+let twoDecimalPoints = numTest.toFixed(2);
+console.log(twoDecimalPoints);
+let stringTest = 'I am the very model of a modern major general';
+stringTest = stringTest.toUpperCase();
+let indexOfM = stringTest.indexOf('M');
+console.log(indexOfM);
+let start = stringTest.indexOf("MODEL");
+let end = stringTest.lastIndexOf('MAJOR');
+let subStr = stringTest.substring(start, end);
+console.log(subStr);
+document.write("<p>" + subStr + "</p>");
